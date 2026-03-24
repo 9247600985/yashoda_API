25041c2823e1d7e401515cc6a45946d333edf3c9
chiranjeevi <chiranjeeviprosoft854@gmail.com>
2026-03-24 17:24:19 +0530

    update

---
+++ src/controllers/Masters/doctorChargeMasterController.ts
@@ -0,0 +1,72 @@
+import { Request, Response, Router } from "express";
+import { executeDbQuery } from "../../db";
+import { authenticateToken } from "../../utilities/authMiddleWare";
+
+export default class doctorChargeMasterController {
+  constructor(private router: Router) {
+    router.use("/masters", this.router);
+    this.router.get("/getTariff", authenticateToken, this.getTariff.bind(this));
+    this.router.get("/getCountries", authenticateToken, this.getCountries.bind(this));
+    this.router.get("/getCurrency", authenticateToken, this.getCurrency.bind(this));
+    this.router.get("/getClinics", authenticateToken, this.getClinics.bind(this));
+  }
+  async getClinics(req: Request, res: Response): Promise<void> {
+    const sql = `select ISNULL(USERID,'')as USERID,SHORTNAME,CLINIC_CODE,CLINIC_NAME,ADDRESS,COUNTRY,STATE,DISTRICT,CITY,EMAIL,PHONE,CONVERT(VARCHAR(10),CONTRACTSDATE,103)StartDate,CONVERT(VARCHAR(10),CONTRACTEDATE,103)EndDate,STATUS from TM_CLINICS`;
+    try {
+      const { records } = await executeDbQuery(sql);
+      res.json({ status: 0, d: records });
+    } catch (err: any) {
+      res.status(500).json({ status: 1, result: err.message });
+    }
+  }
+  async getCurrency(req: Request, res: Response): Promise<void> {
+    const input = req.body || req.query;
+    const Country_ID = input.Country_ID;
+
+    const sql = `select CURRENCY from Mst_Country where Country_ID=@Country_ID`;
+    const params = {
+      Country_ID: Country_ID,
+    };
+    try {
+      const { records } = await executeDbQuery(sql, params);
+      res.json({ status: 0, d: records });
+    } catch (err: any) {
+      res.status(500).json({ status: 1, result: err.message });
+    }
+  }
+  async getTariff(req: Request, res: Response) {
+    const sql = `
+    SELECT 
+      TARIFFID AS Code,
+      TARIFFDESC AS Name
+    FROM MST_TARIFFCATGORY
+    WHERE STATUS = 'A'
+    ORDER BY TARIFFID
+  `;
+
+    try {
+      const { records } = await executeDbQuery(sql);
+
+      res.json({
+        status: 0,
+        d: records,
+      });
+    } catch (err: any) {
+      console.error(err);
+      res.status(500).json({
+        status: 1,
+        message: err.message,
+      });
+    }
+  }
+
+  async getCountries(req: Request, res: Response): Promise<void> {
+    const sql = `select Country_ID,Country_Name,STATUS from Mst_Country order by Country_Name`;
+    try {
+      const { records } = await executeDbQuery(sql);
+      res.json({ status: 0, d: records });
+    } catch (err: any) {
+      res.status(500).json({ status: 1, result: err.message });
+    }
+  }
+}
